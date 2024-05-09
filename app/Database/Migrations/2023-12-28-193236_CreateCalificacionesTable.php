bf536d434d9a69b29d5268a7bf4354fce5ecb356
Angel Sifuentes <angelsifuentes2580@gmail.com>
2024-05-09 14:13:14 -0500

    add_table-calificaciones

---
+++ app/Database/Migrations/2023-12-28-193236_CreateCalificacionesTable.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateCalificacionesTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'comentario' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'calificacion' => [
+                'type' => 'INTEGER',
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP'
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP'
+            ],
+            'id_producto' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+            ],
+            'id_usuario' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+            ]
+        ]);
+
+        $this->forge->addKey('id', true);
+
+        // Agregar claves foráneas
+        $this->forge->addForeignKey('id_producto', 'productos', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('id_usuario', 'usuarios', 'id', 'CASCADE', 'CASCADE');
+
+        $this->forge->createTable('calificaciones');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('calificaciones', true);  // Eliminar si existe para evitar errores
+    }
+}
